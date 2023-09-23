9002de60c2db40fe4c64b9ec81e167cc29341ea9
kevin <kevinparra605@gmail.com>
2023-09-22 19:36:53 -0500

    first commit

---
+++ Taller/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Taller
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello word");
+            Console.ReadKey();
+        }
+    }
+}
